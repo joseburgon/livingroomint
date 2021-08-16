034c22b10043abf5207f21923d4b993d60604ecc
Jose Burgon <jburgon@promarketingchile.com>
2021-08-16 17:49:10 -0400

    Installing Livewire

---
+++ resources/views/donations.blade.php
@@ -0,0 +1,3 @@
+<x-base-layout>
+    <h1>Hi from donations again!</h1>
+</x-base-layout>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/donaciones', function () {
+    return view('donations');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
