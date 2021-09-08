8f57fff299011813f364e7e1448181d2a9da1a54
Jose Burgon <jburgon@promarketingchile.com>
2021-09-08 17:31:54 -0300

    Purging tailwind again

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,13 @@ module.exports = {
             './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
             './storage/framework/views/*.php',
             './resources/views/**/*.blade.php',
-            './resources/views/**/**/*.blade.php',
         ],
         safelist: [
             'text-success-500',
             'text-red-500',
             'text-yellow-500',
             'w-28',
+            'fill-current'
         ]
     },
 
