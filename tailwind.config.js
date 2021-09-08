b1a6cc99c185c3ba854b808c7ecbe91811d09a97
Jose Burgon <jburgon@promarketingchile.com>
2021-09-08 17:19:17 -0300

    Updating tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    purge: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
+    purge: {
+        content: [
+            './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+            './storage/framework/views/*.php',
+            './resources/views/**/*.blade.php',
+            './resources/views/**/**/*.blade.php',
+        ],
+        safelist: [
+            'text-success-500',
+            'text-red-500',
+            'text-yellow-500',
+            'w-28',
+        ]
+    },
 
     theme: {
         extend: {
