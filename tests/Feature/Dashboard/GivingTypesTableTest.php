b28b65288ca1b5c926e1b9fe3137e9caa4ad581e
Jose Burgon <burgonjose@gmail.com>
2021-12-28 12:41:29 -0300

    Finishing GivingTypesTable tests

--- tests/Feature/Dashboard/GivingTypesTableTest.php
+++ tests/Feature/Dashboard/GivingTypesTableTest.php
@@ -3,9 +3,10 @@
 namespace Tests\Feature\Dashboard;
 
 use App\Http\Livewire\Dashboard\GivingTypesTable;
+use App\Models\GivingType;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class GivingTypesTableTest extends TestCase
@@ -22,191 +23,63 @@ class GivingTypesTableTest extends TestCase
     }
 
     /** @test */
-    public function datatables_active_checkbox_works_correctly()
-    {
-        $userA = User::create([
-            'name' => 'User',
-            'email' => 'user@user.com',
-            'password' => bcrypt('password'),
-            'active' => true,
-        ]);
-
-        $userB = User::create([
-            'name' => 'Another',
-            'email' => 'another@another.com',
-            'password' => bcrypt('password'),
-            'active' => false,
-        ]);
-
-        Livewire::test(DataTables::class)
-            ->assertSee($userA->name)
-            ->assertDontSee($userB->name)
-            ->set('active', false)
-            ->assertSee($userB->name)
-            ->assertDontSee($userA->name);
-    }
-
-    /** @test */
     public function datatables_searches_name_correctly()
     {
-        $userA = User::create([
-            'name' => 'User',
-            'email' => 'user@user.com',
-            'password' => bcrypt('password'),
+        $givingTypeA = GivingType::create([
+            'name' => 'Fake Type A',
             'active' => true,
         ]);
 
-        $userB = User::create([
-            'name' => 'Another',
-            'email' => 'another@another.com',
-            'password' => bcrypt('password'),
-            'active' => false,
-        ]);
-
-        Livewire::test(DataTables::class)
-            ->set('search', 'user')
-            ->assertSee($userA->name)
-            ->assertDontSee($userB->name);
-    }
-
-    /** @test */
-    public function datatables_searches_email_correctly()
-    {
-        $userA = User::create([
-            'name' => 'User',
-            'email' => 'user@user.com',
-            'password' => bcrypt('password'),
-            'active' => true,
-        ]);
-
-        $userB = User::create([
-            'name' => 'Another',
-            'email' => 'another@another.com',
-            'password' => bcrypt('password'),
-            'active' => false,
-        ]);
-
-        Livewire::test(DataTables::class)
-            ->set('search', 'user@user.com')
-            ->assertSee($userA->name)
-            ->assertDontSee($userB->name);
+        Livewire::test(GivingTypesTable::class)
+            ->set('search', 'Type A')
+            ->assertSee($givingTypeA->name)
+            ->assertDontSee('non existent type name');
     }
 
     /** @test */
     public function datatables_sorts_name_asc_correctly()
     {
-        $userC = User::create([
-            'name' => 'Cathy C',
-            'email' => 'cathy@cathy.com',
-            'password' => bcrypt('password'),
+        $givingTypeC = GivingType::create([
+            'name' => 'Type C',
             'active' => true,
         ]);
 
-        $userA = User::create([
-            'name' => 'Andre A',
-            'email' => 'andre@andre.com',
-            'password' => bcrypt('password'),
+        $givingTypeA = GivingType::create([
+            'name' => 'Type A',
             'active' => true,
         ]);
 
-        $userB = User::create([
-            'name' => 'Brian B',
-            'email' => 'brian@brian.com',
-            'password' => bcrypt('password'),
+        $givingTypeB = GivingType::create([
+            'name' => 'Type B',
             'active' => true,
         ]);
 
-        Livewire::test(DataTables::class)
+        Livewire::test(GivingTypesTable::class)
             ->call('sortBy', 'name')
-            ->assertSeeInOrder(['Andre A', 'Brian B', 'Cathy C']);
+            ->assertSeeInOrder(['Type A', 'Type B', 'Type C']);
     }
 
     /** @test */
     public function datatables_sorts_name_desc_correctly()
     {
-        $userC = User::create([
-            'name' => 'Cathy C',
-            'email' => 'cathy@cathy.com',
-            'password' => bcrypt('password'),
+        $givingTypeC = GivingType::create([
+            'name' => 'Type C',
             'active' => true,
         ]);
 
-        $userA = User::create([
-            'name' => 'Andre A',
-            'email' => 'andre@andre.com',
-            'password' => bcrypt('password'),
+        $givingTypeA = GivingType::create([
+            'name' => 'Type A',
             'active' => true,
         ]);
 
-        $userB = User::create([
-            'name' => 'Brian B',
-            'email' => 'brian@brian.com',
-            'password' => bcrypt('password'),
+        $givingTypeB = GivingType::create([
+            'name' => 'Type B',
             'active' => true,
         ]);
 
-        Livewire::test(DataTables::class)
+        Livewire::test(GivingTypesTable::class)
             ->call('sortBy', 'name')
             ->call('sortBy', 'name')
-            ->assertSeeInOrder(['Cathy C', 'Brian B', 'Andre A']);
-    }
-
-    /** @test */
-    public function datatables_sorts_email_asc_correctly()
-    {
-        $userC = User::create([
-            'name' => 'Cathy C',
-            'email' => 'cathy@cathy.com',
-            'password' => bcrypt('password'),
-            'active' => true,
-        ]);
-
-        $userA = User::create([
-            'name' => 'Andre A',
-            'email' => 'andre@andre.com',
-            'password' => bcrypt('password'),
-            'active' => true,
-        ]);
-
-        $userB = User::create([
-            'name' => 'Brian B',
-            'email' => 'brian@brian.com',
-            'password' => bcrypt('password'),
-            'active' => true,
-        ]);
-
-        Livewire::test(DataTables::class)
-            ->call('sortBy', 'email')
-            ->assertSeeInOrder(['Andre A', 'Brian B', 'Cathy C']);
-    }
-
-    /** @test */
-    public function datatables_sorts_email_desc_correctly()
-    {
-        $userC = User::create([
-            'name' => 'Cathy C',
-            'email' => 'cathy@cathy.com',
-            'password' => bcrypt('password'),
-            'active' => true,
-        ]);
-
-        $userA = User::create([
-            'name' => 'Andre A',
-            'email' => 'andre@andre.com',
-            'password' => bcrypt('password'),
-            'active' => true,
-        ]);
-
-        $userB = User::create([
-            'name' => 'Brian B',
-            'email' => 'brian@brian.com',
-            'password' => bcrypt('password'),
-            'active' => true,
-        ]);
-
-        Livewire::test(DataTables::class)
-            ->call('sortBy', 'email')
-            ->call('sortBy', 'email')
-            ->assertSeeInOrder(['Cathy C', 'Brian B', 'Andre A']);
+            ->assertSeeInOrder(['Type C', 'Type B', 'Type A']);
     }
 }
